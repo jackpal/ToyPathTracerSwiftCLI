4d35bdb4c124b139c98ca7e2953540ff1c7f34cd
jackpal <jack.palevich@gmail.com>
2022-01-30 23:20:28 -0800

    Use relative path for output file.

--- Sources/ToyPathTracerSwiftCLI/FileUtil.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-func getDocumentsDirectory() -> URL {
-  let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-  return paths[0]
-}
--- Sources/ToyPathTracerSwiftCLI/Tracer.swift
+++ Sources/ToyPathTracerSwiftCLI/Tracer.swift
@@ -20,20 +20,45 @@ struct Tracer: ParsableCommand {
   @Option(help: "lossy compression quality level.")
   var lossyCompressionQuality = 0.76
   
-  @Option(help: "output file name")
+  @Option(help: "output file name. (heic, jpeg, png, tiff)")
   var out = "trace.heic"
 
   mutating func run() throws {
     let buffer = trace(width: width, height: height, frames: frames, threaded: threaded)
     let image = buffer.image()
     
+    let options = [kCGImageDestinationLossyCompressionQuality
+                   as CIImageRepresentationOption : lossyCompressionQuality]
     let context = CIContext()
-    let filename = getDocumentsDirectory().appendingPathComponent(out)
-    print("Writing to \(filename.absoluteString)")
-    try context.writeHEIF10Representation(of: image.settingAlphaOne(in: image.extent),
-                                        to: filename,
-                                        colorSpace: image.colorSpace!,
-                                        options: [kCGImageDestinationLossyCompressionQuality
-                                                  as CIImageRepresentationOption : lossyCompressionQuality])
+    let colorSpace = image.colorSpace!
+    let filename = URL(fileURLWithPath: out)
+    let pathExtension = filename.pathExtension
+    switch pathExtension.lowercased() {
+    case "heic","heif":
+      try context.writeHEIF10Representation(of: image.settingAlphaOne(in: image.extent),
+                                          to: filename,
+                                          colorSpace: colorSpace,
+                                          options: options)
+    case "jpeg", "jpg":
+      try context.writeJPEGRepresentation(of: image,
+                                          to: filename,
+                                          colorSpace: colorSpace,
+                                          options: options)
+
+    case "png":
+      try context.writePNGRepresentation(of: image,
+                                         to: filename,
+                                         format: .RGBA16,
+                                         colorSpace: colorSpace,
+                                         options: options)
+    case "tif", "tiff":
+      try context.writeTIFFRepresentation(of: image,
+                                         to: filename,
+                                         format: .RGBA16,
+                                         colorSpace: colorSpace,
+                                         options: options)
+    default:
+      print("Unknown file extension: \(pathExtension)")
+    }
   }
 }
