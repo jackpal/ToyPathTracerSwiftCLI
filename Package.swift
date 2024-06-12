5b0112a097b8704207ca03dedbb149c7799696f5
Jack Palevich <jack.palevich@gmail.com>
2024-06-11 20:36:03 -0700

    Base MacOS 15.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,14 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ToyPathTracerSwiftCLI",
-    platforms: [
-            .macOS(.v12),
-        ],
+    platforms: [.macOS(.v15)],
     dependencies: [
       .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.2"),
-      .package(url: "https://github.com/jackpal/ToyPathTracerSwift.git", from: "1.0.1"),
+      .package(url: "https://github.com/jackpal/ToyPathTracerSwift.git", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -21,8 +19,6 @@ let package = Package(
               .product(name: "ArgumentParser", package: "swift-argument-parser"),
               "ToyPathTracerSwift"
             ]),
-        .testTarget(
-            name: "ToyPathTracerSwiftCLITests",
-            dependencies: ["ToyPathTracerSwiftCLI"]),
-    ]
+    ],
+    swiftLanguageVersions: [.version("6")]
 )
